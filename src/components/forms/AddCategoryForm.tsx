b901f045f8540952eea43dc74b59ef43c2cbea88
binoy638 <binoy638@gmail.com>
2022-06-19 21:51:54 +0530

    refactor: seperate category form component

---
+++ src/components/forms/AddCategoryForm.tsx
@@ -0,0 +1,57 @@
+import { Button, Input, Loader } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import type { AxiosError } from 'axios';
+import React from 'react';
+import { useMutation, useQueryClient } from 'react-query';
+
+import * as AdminAPI from '@/API/adminAPI';
+
+const AddCategoryForm = () => {
+  const form = useForm({
+    initialValues: {
+      name: '',
+    },
+  });
+
+  const queryClient = useQueryClient();
+
+  const { mutate, isLoading } = useMutation(AdminAPI.addCategory, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('categories');
+    },
+    onError: (err: AxiosError) => {
+      if (err && err?.response?.status === 422) {
+        form.setErrors({ name: 'Category already exists' });
+        return;
+      }
+      form.setErrors({ name: 'Something went wrong' });
+    },
+  });
+
+  const handleSubmit = (values: typeof form.values) => {
+    mutate(values.name);
+  };
+
+  return (
+    <form
+      className="flex w-1/4 flex-col gap-2"
+      onSubmit={form.onSubmit(handleSubmit)}
+    >
+      <Input
+        required
+        placeholder="Category Name"
+        {...form.getInputProps('name')}
+      />
+      {form.errors && form.errors.name && (
+        <small className="text-red-500">{form.errors.name}</small>
+      )}
+      <div>
+        <Button rightIcon={isLoading && <Loader />} type="submit">
+          Add
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export default AddCategoryForm;
--- src/pages/admin/categories.tsx
+++ src/pages/admin/categories.tsx
@@ -1,41 +1,22 @@
 /* eslint-disable no-underscore-dangle */
-import { Button, Input, Loader, Table } from '@mantine/core';
+import { Table } from '@mantine/core';
 import type { JobCategories } from '@types';
-import type { AxiosError } from 'axios';
 import type { GetServerSideProps } from 'next';
 import type { ReactElement } from 'react';
-import React, { useMemo, useState } from 'react';
-import { useMutation, useQuery } from 'react-query';
+import React, { useMemo } from 'react';
+import { useQuery } from 'react-query';
 
+import * as API from '@/API/generalAPI';
+import AddCategoryForm from '@/components/forms/AddCategoryForm';
 import AdminPanelLayout from '@/layouts/AdminPanelLayout';
-
-import * as AdminAPI from '../../../API/adminAPI';
-import * as API from '../../../API/generalAPI';
 
 const AdminJobCategories = ({
   categories,
 }: {
   categories: JobCategories[];
 }) => {
-  const [newCategoryName, setNewCategoryName] = useState('');
-
-  const [error, setError] = useState('');
-
-  const { data, refetch } = useQuery('categories', API.getJobCategories, {
+  const { data } = useQuery('categories', API.getJobCategories, {
     initialData: categories,
-  });
-
-  const { mutate, isLoading } = useMutation(AdminAPI.addCategory, {
-    onSuccess: () => {
-      refetch();
-    },
-    onError: (err: AxiosError) => {
-      if (err && err?.response?.status === 422) {
-        setError('Category already exists');
-      } else {
-        setError('Something went wrong');
-      }
-    },
   });
 
   const rows = useMemo(() => {
@@ -49,16 +30,6 @@ const AdminJobCategories = ({
       : [];
   }, [data]);
 
-  const addCategoryHandler = () => {
-    setError('');
-    if (!newCategoryName) return;
-    mutate(newCategoryName);
-  };
-
-  const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNewCategoryName(event.target.value);
-  };
-
   return (
     <div>
       <Table>
@@ -70,16 +41,7 @@ const AdminJobCategories = ({
         </thead>
         <tbody>{rows}</tbody>
       </Table>
-      <div className="mt-4 flex w-1/4 gap-4">
-        <Input placeholder="Category Name" onChange={inputChangeHandler} />
-        <Button
-          onClick={addCategoryHandler}
-          rightIcon={isLoading && <Loader />}
-        >
-          Add
-        </Button>
-        {error && <small className="text-red-500">{error}</small>}
-      </div>
+      <AddCategoryForm />
     </div>
   );
 };
